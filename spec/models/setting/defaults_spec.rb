5275c6c541b0d954c965a537dc3639bd4f644e5f
Oleksandr Kirpachov <olecsandr.kirpachov@gmail.com>
2024-02-26 18:10:29 +0100

    NEW spec issue: Create Setting with parser information

--- spec/models/setting/defaults_spec.rb
+++ spec/models/setting/defaults_spec.rb
@@ -14,15 +14,16 @@ RSpec.describe Setting::DEFAULTS do
 
   it 'should all be valid Setting' do
     subject.each do |key, setting_data|
-      setting = Setting.new(setting_data.except(:default, :parser).merge(key: key))
-      expect(setting).to be_valid
+      setting = Setting.new(setting_data.except(:default).merge(key: key))
+      setting.validate
       expect(setting.errors).to be_empty
-      expect(setting.save).to be true
+      expect(setting).to be_valid
+      expect(setting.save).to eq true
     end
   end
 
   %i[default_language].each do |key|
-    it "should have a #{key} preference" do
+    it "should have a #{key} setting" do
       expect(subject[key]).to be_a(Hash)
       expect(subject[key.to_s]).to be_a(Hash)
       expect(subject[key.to_sym]).to be_a(Hash)
