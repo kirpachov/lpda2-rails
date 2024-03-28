fc367e45b67917fef962d5296dbb57b957d692c0
Oleksandr Kirpachov <olecsandr.kirpachov@gmail.com>
2024-03-28 21:25:52 +0100

    FIX formatted migration

--- db/migrate/007_create_menu_dishes_in_categories.rb
+++ db/migrate/007_create_menu_dishes_in_categories.rb
@@ -3,10 +3,9 @@
 class CreateMenuDishesInCategories < ActiveRecord::Migration[7.0]
   def change
     create_table :menu_dishes_in_categories, id: :bigint do |t|
-      # t.bigint  :menu_dish_id,       null: false, index: true
-      t.belongs_to :menu_dish, null: false, index: true, foreign_key: { to_table: :menu_dishes }
-      t.bigint  :menu_category_id,   null: true, index: true
-      t.integer :index, null: false, index: true, comment: %(Index of the element in the list. Starts at 0.)
+      t.belongs_to :menu_dish,        null: false, index: true, foreign_key: { to_table: :menu_dishes }
+      t.bigint  :menu_category_id,    null: true, index: true
+      t.integer :index,               null: false, index: true, comment: %(Index of the element in the list. Starts at 0.)
 
       t.timestamps
 
