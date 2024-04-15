9ae1ea8a90aa54ef1e0d9acf671cd28f05b20f05
Oleksandr Kirpachov <olecsandr.kirpachov@gmail.com>
2024-04-15 22:37:33 +0200

    FIX removed comment

--- app/controllers/v1/admin/menu/export_controller.rb
+++ app/controllers/v1/admin/menu/export_controller.rb
@@ -4,7 +4,6 @@ module V1::Admin::Menu
   # Export menu utils
   class ExportController < ApplicationController
     def export
-      # send_data Menu::Ingredient.all.to_csv, filename: "ingredients.csv"
       @file = ::Menu::ExportMenu.run!
 
       send_file @file, filename: "menu.xlsx", type: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
