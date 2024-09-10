08eb733f4e975f8394cc602d99322ccd9d66343c
Oleksandr Kirpachov <olecsandr.kirpachov@gmail.com>
2024-09-10 17:24:28 +0200

    NEW better export db script: can specify output file now

--- scripts/export-db-docker.sh
+++ scripts/export-db-docker.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
 # Run it from the root of the project with
-# ENCRYPTION_PASSWORD=Mario LPDA2_DATABASE_PASSWORD="somethingNooneWillGuess" DATABASE_NAME=lpda2_development ./scripts/export-db-docker.sh
-
-output_filename="backup-$(date +"%Y_%m_%d_%H:%M").sql"
+# ./scripts/export-db-docker.sh [<output_filename>]
+# Example (with environment variables):
+# LPDA2_DATABASE_PASSWORD="somethingNooneWillGuess" DATABASE_NAME=lpda2_development ./scripts/export-db-docker.sh
 
 LPDA2_DATABASE_PASSWORD="${LPDA2_DATABASE_PASSWORD:-somethingNooneWillGuess}"
 DATABASE_NAME="${DATABASE_NAME:-lpda2_development}"
 HOST_WORKSPACE="${HOST_WORKSPACE:-/home/$(whoami)/lpda2-backups}"
-# ENCRYPTION_PASSWORD="${ENCRYPTION_PASSWORD?Please provide ENCRYPTION_PASSWORD to encrypt the backup}"
+OUTPUT_FILE="${1:-$HOST_WORKSPACE/backup-$(date +"%Y_%m_%d_%H:%M").sql}"
+
+output_filename=$(basename $OUTPUT_FILE)
 
 mkdir -p $HOST_WORKSPACE
 
@@ -26,4 +28,6 @@ if [ $? != 0 ]; then
   exit $?
 fi
 
-echo "$HOST_WORKSPACE/$output_filename"
+mv $HOST_WORKSPACE/$output_filename $OUTPUT_FILE
+
+echo "$OUTPUT_FILE"
