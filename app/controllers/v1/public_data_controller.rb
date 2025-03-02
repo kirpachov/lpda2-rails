b84d1a3b1fab85fd270b364853642244d97e18a7
Oleksandr Kirpachov <olecsandr.kirpachov@gmail.com>
2025-03-03 00:15:43 +0100

    FIX do cache only generic data

--- app/controllers/v1/public_data_controller.rb
+++ app/controllers/v1/public_data_controller.rb
@@ -5,18 +5,16 @@ module V1
     skip_before_action :authenticate_user
 
     def index
-      data = cache_action_response do
+      common_data = cache_action_response do
         calc_public_data
       end
 
-      render json: data
-    end
+      reservation = Reservation.public_visible.find_by(
+        secret: cookies[Reservation::PUBLIC_CREATE_COOKIE],
+        datetime: Time.zone.now..
+      )
 
-    def calc_public_data
-      reservation = Reservation.public_visible.where(secret: cookies[Reservation::PUBLIC_CREATE_COOKIE],
-                                                     datetime: Time.zone.now..).first
-
-      {
+      render json: common_data.merge(
         reservation: reservation.as_json(
           only: %w[id fullname datetime status secret children adults notes email phone created_at updated_at],
           include: [
@@ -26,7 +24,12 @@ module V1
               }
             }
           ]
-        ),
+        )
+      )
+    end
+
+    def calc_public_data
+      {
         settings: Setting.all.where(key: Setting::PUBLIC_KEYS).pluck(:key, :value).to_h,
         public_messages: PublicMessage.visible.i18n.pluck(:key, :text).to_h,
         contacts: Contact.public_formatted
