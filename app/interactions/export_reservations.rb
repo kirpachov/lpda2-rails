8cd95a9be6adfcfc3388bbcc7758fa4d4a3a64c0
Oleksandr Kirpachov <olecsandr.kirpachov@gmail.com>
2025-03-09 16:15:54 +0100

    FIX export reservations #find_each

--- app/interactions/export_reservations.rb
+++ app/interactions/export_reservations.rb
@@ -27,12 +27,14 @@ class ExportReservations < ActiveInteraction::Base
                 payment_hpp_url payment_value payment_status
               ])
 
-    reservations.each_with_index do |reservation, index|
+    index = 0
+    reservations.find_each do |reservation|
       write_row(sheet, index + 1,
                 [reservation.id, reservation.fullname, reservation.datetime.strftime("%e/%m/%Y %k:%M").strip, reservation.children, reservation.adults,
                  reservation.email, reservation.phone, reservation.table, reservation.notes, reservation.status, reservation.secret,
                  reservation.created_at.strftime("%e/%m/%Y %k:%M").strip, reservation.updated_at.strftime("%e/%m/%Y %k:%M").strip,
                  reservation.payment&.hpp_url, reservation.payment&.value, reservation.payment&.status].flatten)
+      index += 1
     end
   end
 
