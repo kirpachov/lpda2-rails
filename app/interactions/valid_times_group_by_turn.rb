08f4741081957b6d63ca452c7a21568a3c9decdb
Oleksandr Kirpachov <olecsandr.kirpachov@gmail.com>
2025-03-26 00:13:52 +0100

    NEW returning table type only if still available

--- app/interactions/valid_times_group_by_turn.rb
+++ app/interactions/valid_times_group_by_turn.rb
@@ -1,6 +1,11 @@
 # frozen_string_literal: true
 
 class ValidTimesGroupByTurn < ActiveInteraction::Base
+
+  # people: Integer
+  # How many people are trying to reserve. Optional.
+  # Useful to understand if a TableType can be returned based on availability.
+  # integer :people, default: 0
   interface :params, methods: %i[merge to_h], default: {}
 
   validate do
@@ -20,7 +25,7 @@ class ValidTimesGroupByTurn < ActiveInteraction::Base
   def process_turn(turn)
     turn.as_json.merge(
       valid_times: turn.valid_times(date: params[:date]),
-      preorder_reservation_group: group_json(turn.preorder_reservation_groups.active.first),
+      preorder_reservation_group: group_json(turn),
       messages: turn.reservation_turn_messages.active_at(date).map do |m|
         m.as_json(
           only: %i[id from_date to_date],
@@ -54,17 +59,26 @@ class ValidTimesGroupByTurn < ActiveInteraction::Base
   #     }
   #   ]
   # }
-  def group_json(item)
+  def group_json(turn)
+    item = turn.preorder_reservation_groups.active.first
     return nil if item.nil?
 
-    # table_type_id 
     table_type_to_preorder_reservation_groups = item.table_type_to_preorder_reservation_groups.includes(table_type: [:text_translations, images: [:attached_image_blob]]).map do |join|
+      free_seats = AvailableSeatsForReservationTurnAndPgroup.run!(
+        reservation_turn: turn,
+        pgroup: item,
+        table_type: join.table_type,
+        datetime: date
+      )
+      next nil if free_seats.zero?
+      next nil if params[:people].present? && free_seats < params[:people].to_i
+
       join.as_json(
         only: %i[people_per_turn price table_type_id]
       ).merge(
         table_type: join.table_type.public_json
       )
-    end
+    end.filter(&:present?)
 
     item.as_json(
       methods: %i[message]
--- spec/requests/v2/reservations_controller/valid_times_spec.rb
+++ spec/requests/v2/reservations_controller/valid_times_spec.rb
@@ -344,6 +344,111 @@ RSpec.context "GET /v2/reservations/valid_times", type: :request do
     end
   end
 
+  # Many scenarios:
+  # We'll have only one TableType and only one ReservationTurn.
+  # people_per_turn is the number of people can reserve a specific TableType
+  # new_reservation_size is the number of people that are trying to reserve a TableType right now
+  # existing_reservations is the number of people that already reserved a TableType
+  [
+    {
+      people_per_turn: 10,
+      new_reservation_size: nil,
+      existing_reservations: [{ time: "19:00", adults: 10 }],
+    },
+
+    {
+      people_per_turn: Random.rand(10..11),
+      new_reservation_size: Random.rand(2..10),
+      existing_reservations: [{ time: "19:00", adults: 10 }],
+    },
+
+    {
+      people_per_turn: 11,
+      new_reservation_size: 2,
+      small_enough: 1,
+      existing_reservations: [{ time: "19:00", adults: 10 }],
+    },
+
+    {
+      people_per_turn: 10,
+      new_reservation_size: 6,
+      small_enough: 5,
+      existing_reservations: [{ time: "19:00", adults: 5 }],
+    },
+
+    {
+      people_per_turn: 10,
+      new_reservation_size: 3,
+      small_enough: 2,
+      existing_reservations: [{ time: "19:00", adults: 2 }, { time: "19:30", adults: 2 }, { time: "20:00", adults: 2 }, { time: "20:30", adults: 2 }],
+    },
+  ].each do |scenario|
+    context "when turn has table_types but they are already full (all seats are taken) (scenario=#{scenario.inspect})" do
+      subject(:turn) { json[:turns].find { |j| j["starts_at"].include?("19:00") } }
+
+      let(:date) { Time.zone.now.to_date.to_s }
+
+      let(:table_type) { create(:table_type, :with_image) }
+      let(:turns) do
+        [
+          ReservationTurn.create!(name: "Night", weekday: Time.zone.now.wday, starts_at: "19:00", ends_at: "21:00", step: 30),
+        ]
+      end
+
+      let!(:group) do
+        create(:preorder_reservation_group).tap do |grp|
+          grp.add_table_type(table_type: table_type, people_per_turn: scenario[:people_per_turn], price: 4)
+          grp.turns = turns
+        end
+      end
+
+      let(:people) do
+        scenario[:new_reservation_size]
+      end
+
+      before do
+        scenario[:existing_reservations].each do |res|
+          reservation = create(:reservation, table_type: table_type, adults: res[:adults], children: 0, datetime: DateTime.parse("#{date} #{res[:time]}"))
+
+          expect(reservation.table_type).to eq(table_type)
+          expect(reservation.reservation_turn).to eq(turns[0])
+        end
+
+        travel_to Time.zone.now.beginning_of_day do
+          req(date: Time.zone.now.to_date.to_s, people: people)
+        end
+      end
+
+      it { expect(response).to have_http_status(:ok) }
+      it { expect(json).not_to include(message: String) }
+
+      it { expect(json).to include(turns: Array) }
+      it { expect(json[:turns].length).to eq(1) }
+      it { expect(json[:turns][0]).to include(preorder_reservation_group: Hash) }
+
+      it do
+        expect(turn.dig("preorder_reservation_group", "table_type_to_preorder_reservation_groups")).to be_empty
+      end
+
+      if scenario[:small_enough]
+        context "when requested number of people is small enough" do
+          let(:people) { scenario[:small_enough] }
+
+          it { expect(response).to have_http_status(:ok) }
+          it { expect(json).not_to include(message: String) }
+
+          it { expect(json).to include(turns: Array) }
+          it { expect(json[:turns].length).to eq(1) }
+          it { expect(json[:turns][0]).to include(preorder_reservation_group: Hash) }
+
+          it do
+            expect(turn.dig("preorder_reservation_group", "table_type_to_preorder_reservation_groups")).not_to be_empty
+          end
+        end
+      end
+    end
+  end
+
   context "when turn has table_types associated, the active ones should be returned" do
     subject(:turn) { json[:turns].find { |j| j["starts_at"].include?("12:00") } }
 
