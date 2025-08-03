f6497c398615f56ef0055d1aa15052114171d442
Oleksandr Kirpachov <o.kirpachov@opinioni.net>
2025-08-03 21:11:07 +0200

    FIX don't expire non-open checkout session

--- app/interactions/stripe/expire_checkout_session.rb
+++ app/interactions/stripe/expire_checkout_session.rb
@@ -14,11 +14,22 @@ module Stripe
     validates :checkout_session_id, presence: true
 
     def execute
+      return "checkout session is not open" if checkout_session.status != "open"
+
       Stripe::Checkout::Session.expire(
         checkout_session_id
       )
     rescue Stripe::InvalidRequestError => e
       errors.add(:base, "Stripe API error: #{e.message}")
+    end
+
+    def checkout_session
+      @checkout_session ||= Stripe::Checkout::Session.retrieve(
+        checkout_session_id
+      )
+    rescue Stripe::InvalidRequestError => e
+      errors.add(:base, "Stripe API error: #{e.message}")
+      nil
     end
   end
 end
